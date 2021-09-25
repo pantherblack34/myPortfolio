813d162dc177461b60130958d9c5b32cfc05348d
pantherblack34 <89083500+pantherblack34@users.noreply.github.com>
2021-09-25 22:43:44 +0530

    updated

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let part1 = document.querySelector('.part1');
-let part2 = document.querySelector('.part2').style.display = false;
-let part3 = document.querySelector('.part3').style.display = false;
-let part4 = document.querySelector('.part4').style.display = false;
-let part5 = document.querySelector('.part5').style.display = false;
+let part2 = document.querySelector('.part2').style.display = none;
+let part3 = document.querySelector('.part3').style.display = none;
+let part4 = document.querySelector('.part4').style.display = none;
+let part5 = document.querySelector('.part5').style.display = none;
 
 
