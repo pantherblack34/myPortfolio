6336425098e2497424804181d12c9615abbe04cd
pantherblack34 <89083500+pantherblack34@users.noreply.github.com>
2021-09-26 00:39:22 +0530

    updated

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-let part1 = document.querySelector('.part1');
-let part2 = document.querySelector('.part2').style.display = none;
-let part3 = document.querySelector('.part3').style.display = none;
-let part4 = document.querySelector('.part4').style.display = none;
-let part5 = document.querySelector('.part5').style.display = none;
 
+function next(){
+    let partall = document.querySelectorAll('.partall');
 
+    if (partall.style.display = 'none'){
+        partall.style.display = 'block';
+    }
+}
